286c696bdcd401a5540d325037efb531492edb8d
Fredrik Fahlstad <fredrik@fahlstad.se>
2014-11-29 13:06:19 +0100

    config

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 's6411');
+define('DB_NAME', 's6411_wp');
 
 /** MySQL database username */
 define('DB_USER', 's6411');
