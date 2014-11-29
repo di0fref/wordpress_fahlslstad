f810b209fe4103bec77ffb50fe7914cc88ddfdbb
Fredrik Fahlstad <fredrik@fahlstad.se>
2014-11-29 12:52:15 +0100

    config

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp');
+define('DB_NAME', 's6411');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 's6411');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Tfqnm');
 
 /** MySQL hostname */
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'sql.i8t.com');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
